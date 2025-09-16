eb86d4222cca9ad487c88afc34be2090207b2693
bbctothemoon <simonleung213@gmail.com>
2025-09-16 13:42:16 +0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: '/sleep-chronotype-advisor/',
   server: {
     host: "::",
     port: 8080,
